1b745f9a76488fd4ce5fc7eb3a50e6260cf03fef
manaranjoc <angel.naranjo@udea.edu.co>
2022-03-19 09:27:01 -0500

    Add basic login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const login = async () => {
+    try {
+      const loginSuccesful = await axios.get("http://localhost:8080/user/login", {params: {username, password}});
+      if (loginSuccesful.status === 200) {
+        setMessage('Login was succesful')
+      } else {
+        setMessage('Login failes: Review password or username');
+      }
+    } catch (err) {
+      if (err?.response?.status === 404) {
+        setMessage('Login failes: Review password or username');
+      } else {
+        setMessage('error occured in login');
+      }
+    }
+  }
+
+  const register = async () => {
+    try {
+      const newUser = await axios.post("http://localhost:8080/user", {username, password});
+      console.dir(newUser);
+      if (newUser.status === 200) {
+        setMessage('Register was succesful')
+      } else {
+        setMessage('Register failes: Review password or username');
+      }
+    } catch (err) {
+      setMessage('error occured in register');
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input id="username" value={username} onChange={(event) => {setUsername(event.target.value)}} type="text" placeholder="Enter your username" />
+      <input id="password" valuer={password} onChange={(event) => {setPassword(event.target.value)}} type="password" placeholder="Enter your password" />
+      <button onClick={login}>Login</button>
+      <button onClick={register}>Register</button>
+      {message && (<div>{message}</div>)}
     </div>
   );
 }
